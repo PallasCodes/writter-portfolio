b6831d90bb0b441dbd6dd7ad46827d6507a09a79
pallascodes <pallascodes@gmail.com>
2021-10-28 18:30:41 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      white: colors.white,
+      cyan: colors.cyan,
+      lime: colors.lime,
+      gray: colors.gray
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
